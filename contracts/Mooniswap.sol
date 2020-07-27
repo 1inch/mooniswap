5a3c57f093c24c140e7a157d3d3c0099460f4a9c
Anton Bukov <k06aaa@gmail.com>
2020-07-27 21:48:20 +0300

    Move Sqrt.sol into library subdirectory

--- contracts/Mooniswap.sol
+++ contracts/Mooniswap.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "./Sqrt.sol";
+import "./libraries/Sqrt.sol";
 
 
 library VirtualBalance {
