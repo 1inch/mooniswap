505b46d471a46cddc582e5fadda8f1928a6d4eef
Anton Bukov <k06aaa@gmail.com>
2020-07-28 14:32:55 +0300

    Fresh test and coverage config

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,4 @@
 module.exports = {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-    // to customize your Truffle configuration!
-    networks: {
-        development: {
-            host: 'localhost',
-            port: 9545,
-            network_id: '*',
-            gas: 8000000,
-            gasPrice: 1000000000, // web3.eth.gasPrice
-        },
-        coverage: {
-            host: 'localhost',
-            port: 8555,
-            network_id: '*',
-            gas: 8000000,
-            gasPrice: 1000000000, // web3.eth.gasPrice
-        }
-    },
     compilers: {
         solc: {
             version: '0.6.12',
