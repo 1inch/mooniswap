a3f22e586082a1cfcb7cc1b583a7bd78dbf8c812
Mikhail Melnik <by.zumzoom@gmail.com>
2020-07-28 14:40:09 +0300

    remove coverage rpc hack

--- test/Mooniswap.js
+++ test/Mooniswap.js
@@ -17,11 +17,7 @@ async function trackReceivedToken (token, wallet, txPromise) {
             : await token.balanceOf(wallet),
     );
 
-    let txResult = await txPromise();
-    if (txResult.receipt) {
-        // Fix coverage since testrpc-sc gives: { tx: ..., receipt: ...}
-        txResult = txResult.receipt;
-    }
+    const txResult = await txPromise();
     let txFees = web3.utils.toBN('0');
     if (wallet.toLowerCase() === txResult.from.toLowerCase() && token === constants.ZERO_ADDRESS) {
         const receipt = await web3.eth.getTransactionReceipt(txResult.transactionHash);
